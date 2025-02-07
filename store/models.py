330b46949a23a208004877d069504067e97b046a
Parimal Ahire <parimalahire18@gmail.com>
2025-02-07 19:29:30 +0530

    Broke down the templates

--- store/models.py
+++ store/models.py
@@ -42,6 +42,8 @@ class Product(models.Model):
     image= models.ImageField(upload_to='product_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name

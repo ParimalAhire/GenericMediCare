404ecbed6eb26ca896d5196e893d56d42358d3ad
Parimal Ahire <parimalahire18@gmail.com>
2025-02-07 20:19:08 +0530

    Added login and logout

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Product
 
 def home(request):
@@ -7,3 +9,26 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, 'You have been successfully logged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
+
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been successfully logged out')
+    return redirect('home')
